2bdc273f84e914ba7c40c614b57d7f963ebcf0d3
MSib <msib.dev@mail.ru>
2025-03-09 21:27:36 +0500

    add store

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import 'primeicons/primeicons.css'
 import App from './App.vue'
@@ -7,8 +8,10 @@ import { CustomAura } from './theme.ts'
 import ToastService from 'primevue/toastservice'
 import router from './router'
 
+const pinia = createPinia()
 createApp(App)
   .use(router)
+  .use(pinia)
   .use(PrimeVue, {
     theme: {
       preset: CustomAura,
