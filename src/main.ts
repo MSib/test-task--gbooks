e6b4330f2950401f13e687a6775af3b8e0472b8a
MSib <msib.dev@mail.ru>
2025-03-09 20:37:37 +0500

    add ui

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'primeicons/primeicons.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import { CustomAura } from './theme.ts'
+import ToastService from 'primevue/toastservice'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(PrimeVue, {
+    theme: {
+      preset: CustomAura,
+      options: {
+        cssLayer: {
+          name: 'primevue',
+          order: 'base, primevue',
+        },
+      },
+    },
+  })
+  .use(ToastService)
+  .mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'node:url'
+import Components from 'unplugin-vue-components/vite'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
